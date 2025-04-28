da54ee71f665537983b51d81e9c3499643189997
NanoAzad <azad.alli@iitb.ac.in>
2025-04-28 18:29:49 +0530

    Delete app/ components/ui directory

--- app/ components/ui/card.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export function Card({ children, className }) {
-  return <div className={`bg-white rounded-xl p-4 shadow ${className}`}>{children}</div>;
-}
-
-export function CardContent({ children, className }) {
-  return <div className={className}>{children}</div>;
-}
